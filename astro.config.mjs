e41ec382826b63b21a4cb01ffab25d5dfdf3ec98
Иванов Сергей <sergey.e.ivanov@gazprombank.ru>
2024-10-21 22:29:48 +0300

    feat: gh pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://seri0zha.github.io",
   vite: {
     css: {
       preprocessorOptions: {
